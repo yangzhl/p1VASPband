3bfd235978656446be7279dfa7a844c15993fd98
yangzhl <yangzhl@yangzhls-MacBook-Air.local>
2018-10-04 16:12:11 +0800

    modify

--- p1VASPband.py
+++ p1VASPband.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# modified by yang zhi long 
+# email: yangzhilong26@gmail.com
+# time:2018.9.29
 
 # Module imports
 from __future__ import print_function
@@ -10,6 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import matplotlib as mpl
+import math
 #==============================================================================
 
 # Parse Arguments
@@ -20,9 +24,6 @@ parser.add_argument('-o', '--output',
                     help='Output base filename', default='band')
 parser.add_argument('-f', '--format', nargs='+',
                     help='A list of figure formats', default=['pdf'])
-
-parser.add_argument('-d', '--direction',
-                    help='High symmetric direction', default='z')
 parser.add_argument('-b', '--below',
                    help='Lower energy bound below Fermi level', type=float,
                    default=5.0)
@@ -32,16 +33,20 @@ parser.add_argument('-a', '--above',
 parser.add_argument('-s', '--show',
                    help='show interactive band structures',
                    action='store_true')
+parser.add_argument('-v', '--verbose',
+                   help="The level of verbosity of the output: from 1 to 3 ",default='1')
+parser.add_argument('-m', '--mode',
+                   help="band plot mode: adjoin or split ",default='adjoin')
 args = parser.parse_args()
 
 path = args.path
 outcar = os.path.join(path, 'OUTCAR')
 eigval = os.path.join(path, 'EIGENVAL')
 of_dat = os.path.join(path, args.output+'.'+'dat')
-directions = {'x':0, 'y':1, 'z':2}
-direction = directions[args.direction]
 lb = args.below
 ub = args.above
+verbose = args.verbose
+mode = args.mode
 
 # Read Fermi energy and nspin from OUTCAR
 try:
@@ -64,14 +69,19 @@ else:
     raise SystemExit
 nspin = int(l.split()[2])
 
-while 1:
-    l = outcar_fh.next()
+if verbose == "3":
+    print("outcar_fh type: ",type(outcar_fh))
+
+# search in reverse order
+for l in reversed(open("outcar").readlines()):        
     if l.startswith(' E-fermi'):
-        break
-else:
-    print('E-fermi value not found in {}, exit...'.format(outcar))
-    raise SystemExit
+            break
+#else:
+#    print('E-fermi value not found in {}, exit...'.format(outcar))
+#    raise SystemExit
 EFermi = float(l.split()[2])
+if verbose == "2":
+    print("Fermi energy",EFermi)
 
 outcar_fh.close()
 
@@ -80,19 +90,40 @@ for i in range(5):
     eigval_fh.next()
 
 l = eigval_fh.next()
-dump, nk, nb = [int(x) for x in l.split()]
+# number of electrons, kpoints , bands
+nelect, nk, nb = [int(x) for x in l.split()]
 
 eig = np.zeros([nb, nk, nspin])
 kpts = np.zeros([nk,3])
+kpath = np.zeros([nk])
+
+# need to improved
+insect= input("the number of intersections along high symmetry lines? \n")
+klen = 0.0 
 
 for ik in range(nk):
     eigval_fh.next()
     l = eigval_fh.next()
     kpts[ik] = [float(x) for x in l.split()[:3]]
+    if (ik == 0 ): 
+        klen = klen+0.0
+    elif (ik+1)%insect == 1:
+        if mode == "adjoin":
+            klen = klen+0.0
+        elif mode == "split":   # reset kpath
+            klen = 0.0
+        else:
+            print("band plot mode wrong ! ")
+            raise SystemExit
+    else:
+        klen = klen+math.sqrt((kpts[ik,0]-kpts[ik-1,0])**2+(kpts[ik,1]-kpts[ik-1,1])**2+(kpts[ik,2]-kpts[ik-1,2])**2)
+    kpath[ik] = klen
     for ib in range(nb):
         l = eigval_fh.next()
         eig[ib,ik] = [float(x) for x in l.split()[1:]]
 eigval_fh.close()
+
+# fermi level correct
 eig -= EFermi
 
 
@@ -104,7 +135,7 @@ fmt_str = "{:15.7E}" + "{:14.6F}" * nspin + '\n'
 for ib in range(nb):
     for ik in range(nk):
         l = fmt_str.format(
-            kpts[ik,direction],
+            kpath[ik],
             *eig[ib,ik]
         )
         of_fh.write(l)
@@ -112,17 +143,17 @@ for ib in range(nb):
 of_fh.close()
 
 # check high symmetric direction and raise warning if necessary
-small = 1.0E-4
-if abs(kpts[0,direction] - kpts[-1,direction]) < small:
-    print('Warning: K-Points coordinates does not change as expected.')
-    print('         I am treating with the {} direction in the k-space'.format(
-        args.direction))
-    print('         cooresponding to Column {} of the kpts coordiantes.'.format(
-        direction))
-    print("         I've written data into the output file:\n\n\t{}".format(of_dat))
-    print('\nYou can check the help info for details: {} -h'.format(sys.argv[0]))
-    print('\nIt is too difficult to plot such a band structure...\nsorry,\nbye...')
-    raise SystemExit
+#small = 1.0E-4
+#if abs(kpts[0,2] - kpts[-1,2]) < small:
+#    print('Warning: K-Points coordinates does not change as expected.')
+#    print('         I am treating with the {} direction in the k-space'.format(
+#        args.direction))
+#    print('         cooresponding to Column {} of the kpts coordiantes.'.format(
+#        direction))
+#    print("         I've written data into the output file:\n\n\t{}".format(of_dat))
+#    print('\nYou can check the help info for details: {} -h'.format(sys.argv[0]))
+#    print('\nIt is too difficult to plot such a band structure...\nsorry,\nbye...')
+#    raise SystemExit
 
 # plot band structure
 #mpl.rc('text',usetex=True)
@@ -139,19 +170,23 @@ fig.patch.set_alpha(0)
 ax.patch.set_alpha(0)
 colors = ['r','b']
 
-ax.set_xlim([kpts[0,direction], kpts[-1,direction]])
+if verbose == '2':
+    print("xmin: ",kpath[0],"xmax: ",kpath[-1])
+
+ax.set_xlim([kpath[0], kpath[-1]])
 ax.set_ylim([-lb, ub])
 
 for ispin in range(nspin):
     for ib in range(nb):
-        ax.plot(kpts[:,direction],eig[ib,:,ispin], '{}'.format(colors[ispin]),
+        ax.plot(kpath[:],eig[ib,:,ispin], '{}'.format(colors[ispin]),
                linewidth=1, alpha=0.8)
 
 for y in range(int(-lb+1),int(+ub)):
-    ax.plot(kpts[(0,-1),direction],[y,y],'k--',linewidth=0.5,alpha=0.5)
+    ax.plot([kpath[0],kpath[-1]],[y,y],'k--',linewidth=0.5,alpha=0.5)
 
-ax.set_xticks(kpts[(0,-1),direction])
-ax.set_xticklabels([r'$\Gamma$', r'$\mathrm{X}$'])
+ax.set_xticks([kpath[0],0.5,kpath[-1]])
+#ax.set_xticklabels([r'$\Gamma$', r'$\mathrm{X}$'])
+ax.set_xticklabels([r'$\mathrm{-X}$',r'$\Gamma$', r'$\mathrm{X}$'])
 ax.set_yticks(range(int(-lb+1),int(+ub),2))
 ax.set_yticklabels(range(int(-lb+1),int(+ub),2))
 mly = MultipleLocator(0.5)
